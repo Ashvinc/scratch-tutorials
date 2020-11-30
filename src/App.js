ee5800cbb590c2dc0e760457f8637e6602900b46
Ashvin Chellasamy <ashvinchellasamy@Ashvins-MBP.attlocal.net>
2020-11-29 17:17:53 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
+import pentagon from './images/pentagon.png'
+import square from './images/square.png'
+import triangle from './images/triangle.png'
+import start from './images/start.png'
+import minimize from './images/minimize.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Creating Shapes with Scratch
+        </h1>
+        <br/>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://snap.berkeley.edu/snap/snap.html#present:Username=ashvinccccc&ProjectName=Base%20Template"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Starter Code (use this to start off building the projects)
         </a>
+        <p>Press the <img style={{'vertical-align':'middle'}} src={minimize} /> to start coding and the <img style={{'vertical-align':'middle'}} src={start} /> to run the code</p>
+        <br/>
+        <br/>
+        <br/>
+
+        <div className="assignment">
+        <p>
+          Assignment #1 : Create a Triangle
+          <br/>    
+          <a
+          href="https://snap.berkeley.edu/snap/snap.html#present:Username=ashvinccccc&ProjectName=Triangle"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          [SOLUTION]
+        </a>
+        </p>
+        <img src={triangle} className="example" alt="triangle" />
+        </div>
+
+        <div className="assignment">
+        <p>
+          Assignment #2 : Create a Square 
+          <br/>  
+          <a
+          href="https://snap.berkeley.edu/snap/snap.html#present:Username=ashvinccccc&ProjectName=Square"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          [SOLUTION]
+        </a> 
+        </p>
+        <img src={square} className="example" alt="square" />
+        </div>
+
+        <div className="assignment">
+        <p>
+          
+          Assignment #3 : Create a Pentagon 
+          <br/>  
+          <a
+          href="https://snap.berkeley.edu/snap/snap.html#present:Username=ashvinccccc&ProjectName=Pentagon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          [SOLUTION]
+        </a>
+          </p>
+        <img src={pentagon} className="example" alt="pentagon" />
+        </div>
+
+
       </header>
     </div>
   );
